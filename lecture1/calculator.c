5323950e2aff404a2e121a6b5d433ba23ca77237
jorge <62824468+jotavev@users.noreply.github.com>
2022-12-23 23:24:44 -0300

    parity

--- lecture1/calculator.c
+++ lecture1/calculator.c
@@ -3,9 +3,9 @@
 
 int main(void)
 {
-    int x = get_int("x: ");
-    int y = get_int("y: ");
+    long x = get_long("x: ");
+    long y = get_long("y: ");
 
-    printf("%i\n", x + y);
+    printf("%li\n", x + y);
     
 }
