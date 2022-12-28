c8c9cbe24c1269e0c7f32bcb33a571a3495abdc9
jorge <62824468+jotavev@users.noreply.github.com>
2022-12-27 22:04:51 -0300

    mario1done

---
+++ lecture1/pset1/mario.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void printer(int size);
+void printblank(int n);
+void printhash(int n);
+
+int main(void)
+{
+    int heigth;
+    do
+    {
+        heigth = get_int("Heigth: ");
+    } 
+    while (heigth < 1 || heigth > 8);
+
+    printer(heigth);
+}
+
+void printer(int size)
+{
+    int printBlankTimes= size;
+    int printHashTimes = 1;
+    for (int i = 0; i < size; i++)
+    {
+        printBlankTimes--;
+        printblank(printBlankTimes);
+        printhash(printHashTimes);
+        printHashTimes++;
+
+        printf("\n");
+    }
+}
+
+void printblank(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf(" ");
+    }
+}
+    
+void printhash(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+}
+
