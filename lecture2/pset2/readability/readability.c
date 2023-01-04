2efbddc8b46e26edca5d234434aa106d1cda3e3d
jorge <62824468+jotavev@users.noreply.github.com>
2023-01-04 14:53:28 -0300

    readability_V2.0

--- lecture2/pset2/readability/readability.c
+++ lecture2/pset2/readability/readability.c
@@ -5,10 +5,10 @@
 
 float avarage_per_100(float number, float word);
 
-int main(void) 
+int main(void)
 {
     string text = get_string("Text: ");
-    
+
     int letter = 0;
     int word = 1;
     int sentece = 0;
@@ -30,26 +30,21 @@ int main(void)
             sentece++;
         }
     }
-    
+
     result = 0.0588 * avarage_per_100(letter, word) - 0.296 * avarage_per_100(sentece, word) - 15.8;
 
-    printf("letter: %i\n", letter);
-    printf("word: %i\n", word);
-    printf("sentece: %i\n", sentece);
     if ((int) round(result) > 16)
     {
-        printf("Grade: 16+\n");
+        printf("Grade 16+\n");
+    }
+    else if ((int) round(result) < 1)
+    {
+        printf("Before Grade 1");
     }
     else
     {
-        printf("Grade: %i\n",(int) round(result));
+        printf("Grade %i\n",(int) round(result));
     }
-
-
-    printf("test1 = EXPECTED: 464.29 | RESULT: %f\n", avarage_per_100(65, 14));
-    printf("test2 = EXPECTED: 28.57 | RESULT: %f\n", avarage_per_100(4, 14));
-    printf("test3= EXPECTED: 382.14 | RESULT: %f\n", avarage_per_100(214, 56));
-    printf("test4 = EXPECTED: 7.14 | RESULT: %f\n", avarage_per_100(4, 56));
 
 }
 
@@ -59,7 +54,3 @@ float avarage_per_100(float number, float word)
     result = number / word * 100;
     return result;
 }
-
-// coleman-liau is: index = 0.0588 * L - 0.296 * S 15.8
-// L is avarege number of letters per 100 words in the text
-// S is avarage number of senteces per 100 words in the text
