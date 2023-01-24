bfaf3735376479105c53a3175aa211e33a7c637c
jorge <62824468+jotavev@users.noreply.github.com>
2023-01-24 14:59:59 -0300

    memory

--- lecture4/examples/address2.c
+++ lecture4/examples/address2.c
@@ -2,13 +2,8 @@
 
 int main(void)
 {
-    char *s = "HI!";
-    printf("%p\n", &s);
-    printf("%c\n", *s);
-    printf("%c\n", *(s+1));
-    printf("%c\n", *(s+2));
-    printf("%c\n", *(s+3));
-    printf("%c\n", *(s+4));
-    printf("%c\n", *(s+5));
-    printf("%c\n", *(s+6));
+    int n = 50;
+    int *p = &n;
+    printf("%p\n", p);
+    printf("%i\n", *p);
 }
