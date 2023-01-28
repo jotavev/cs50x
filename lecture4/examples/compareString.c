30e11a4372615b230db4aa803639cda328c08bf3
jorge <62824468+jotavev@users.noreply.github.com>
2023-01-27 21:10:58 -0300

    memory

--- lecture4/examples/compareString.c
+++ lecture4/examples/compareString.c
@@ -1,17 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     char *s = get_string("s: ");
     char *t = get_string("t: ");
 
-    if (s == t)
+    if (strcmp(s, t) == 0)
     {
-        printf("Same\n");
+        printf("same\n");
     }
     else 
     {
-        printf("Different\n");
+        printf("different\n");
     }
 }
