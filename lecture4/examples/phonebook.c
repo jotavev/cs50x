f6ddfb02c6a70e3247e176a67317b27c6e16be31
jorge <62824468+jotavev@users.noreply.github.com>
2023-02-02 14:52:58 -0300

    understandswapandpointerthankyougod

---
+++ lecture4/examples/phonebook.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // Open CSV file
+    FILE *file = fopen("phonebook.csv", "a");
+    if (!file)
+    {
+        return 1;
+    }
+  
+    // Get name and number
+    string name = get_string("Name: ");
+    string number = get_string("Number: ");
+  
+    // Print to file
+    fprintf(file, "%s,%s\n", name, number);
+  
+    // Close file
+    fclose(file);
+}
