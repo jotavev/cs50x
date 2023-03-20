403f4f27a9081fc2f37b733e291c623ef9f54272
jorge_thinkpad <62824468+jotavev@users.noreply.github.com>
2023-03-20 11:54:10 -0300

    dataStructures

--- lecture5/lab5/inheritance/inheritance.c
+++ lecture5/lab5/inheritance/inheritance.c
@@ -50,12 +50,12 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        parent0->parents[0] = p;
-        parent1->parents[0] = p;
+        p->parents[0] = parent0;
+        p->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        p->alleles[0] = random_allele();
-        p->alleles[1] = random_allele();
+        p->alleles[0] = p->parents[0]->alleles[rand() % 2];
+        p->alleles[1] = p->parents[1]->alleles[rand() % 2];
 
     }
 
@@ -72,17 +72,24 @@ person *create_family(int generations)
     }
 
     // TODO: Return newly created person
-    return NULL;
+    return p;
 }
 
 // Free `p` and all ancestors of `p`.
 void free_family(person *p)
 {
     // TODO: Handle base case
+    if (p == NULL)
+    {
+        return;
+    }
 
     // TODO: Free parents recursively
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
 
     // TODO: Free child
+    free(p);
 
 }
 
