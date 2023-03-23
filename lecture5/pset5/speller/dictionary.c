76f258d7678a9fbc57e558c72870a0c1bd92e709
jorge <62824468+jotavev@users.noreply.github.com>
2023-03-23 15:17:13 -0300

    speller.c compiles but have bugs

--- lecture5/pset5/speller/dictionary.c
+++ lecture5/pset5/speller/dictionary.c
@@ -2,6 +2,9 @@
 
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "dictionary.h"
 
@@ -14,7 +17,10 @@ typedef struct node
 node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
+// const unsigned int N = 26;
+#define N 2600
+
+int sum = 0;
 
 // Hash table
 node *table[N];
@@ -23,6 +29,14 @@ node *table[N];
 bool check(const char *word)
 {
     // TODO
+    int h = hash(word);
+    for (node *tmp = table[h]; tmp != NULL; tmp = tmp->next)
+    {
+        if (strcmp(tmp->word, word) == 0)
+        {
+            return true;
+        }
+    }
     return false;
 }
 
@@ -30,26 +44,73 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    // polynomial accumulation using horner's rule 
+    int h = word[0];
+    for (int i = 1; word[i] != '\0'; i++)
+    {
+        h += h * 33 + word[i];
+    }
+    return h % N;
+    // return toupper(word[0]) - 'A';
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        return false;
+    }
+    char word[LENGTH + 1];
+    while(fscanf(file, "%s", word) != EOF)
+    {
+        node *n = malloc(sizeof(node));
+
+        if (n == NULL)
+        {
+            free(n);
+            return false;
+        }
+
+        int h = hash(word);
+
+        strcpy(n->word, word);
+        n->next = table[h];
+        table[h] = n;
+        sum++;
+    }
+    return true;
     // TODO
-    return false;
+    // return false;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
     // TODO
-    return 0;
+    return sum;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
     // TODO
-    return false;
+    for (int i = 0; i < N; i++)
+    {
+        while (table[i] != NULL)
+        {
+            node *tmp = table[i]->next;
+            free(table[i]);
+            table[i] = tmp;
+        }
+    }
+    for (int i = 0; i < N; i++)
+    {
+        if (table[i] != NULL)
+        {
+            return false;
+        }
+    }
+    return true;
 }
