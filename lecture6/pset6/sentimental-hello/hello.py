331239e7a2745e64c61d949f3dba8146c307f91a
jorge <62824468+jotavev@users.noreply.github.com>
2023-03-24 12:17:27 -0300

    sentimental-mario-done

---
+++ lecture6/pset6/sentimental-hello/hello.py
@@ -0,0 +1,6 @@
+import sys
+print("What is your name?")
+name = sys.stdin.readline()
+print(f"hello, {name}")
+
+# name = str(input("What is your name?" end=''))
---
+++ lecture6/pset6/sentimental-mario-more/mario.py
@@ -0,0 +1,35 @@
+# import get_int
+from cs50 import get_int
+
+
+def main():
+    bricks = 1
+    height = get_height()
+
+    # loop for printer mario bricks
+    for i in range(height):
+        height -= 1
+        printer(" ", height)
+        printer("#", bricks)
+        printer(" ", 2)
+        printer("#", bricks)
+        bricks += 1
+        print()
+
+
+# get a height value of user
+def get_height():
+    while True:
+        height = get_int("Height: ")
+        if height > 0 and height < 9:
+            break
+    return height
+
+
+# printer function
+def printer(char, num):
+    for i in range(num):
+        print(f"{char}", end='')
+
+
+main()
