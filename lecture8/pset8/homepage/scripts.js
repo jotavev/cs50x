7a561bf20d01d8e2c1a2aee5c953065ecb72eda9
jorge_thinkpad <62824468+jotavev@users.noreply.github.com>
2023-04-20 18:34:54 -0300

    pset8 v2.0

---
+++ lecture8/pset8/homepage/scripts.js
@@ -0,0 +1,35 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+
+function showModal() {
+    const myModal = document.getElementById('modal');
+    const modal = new bootstrap.Modal(myModal);
+    modal.show();
+}
+
+function checkUrlToShowModal() {
+  var url = window.location.href;
+  if (url.endsWith("?")) {
+    showModal();
+  }
+}
+
+window.onload = function() {
+  checkUrlToShowModal();
+}
