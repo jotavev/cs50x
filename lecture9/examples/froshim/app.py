dac03df429fbf3b07188332afc02d59827c5a35c
jorge <62824468+jotavev@users.noreply.github.com>
2023-04-28 21:10:54 -0300

    lecture9 update

---
+++ lecture9/examples/froshim/app.py
@@ -0,0 +1,20 @@
+from types import MethodType
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["POST"])
+def register():
+
+    # Validade submission
+    if not request.form.get("name") or request.form.get("sport") not in ["Basketball", "Soccer", "Ultimate Frisbee"]:
+        return render_template("failure.html")
+
+    # Confirm registration
+    return render_template("sucess.html")
