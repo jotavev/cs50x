89372d4c15007cf454e15a73015178a50195f091
jorge <62824468+jotavev@users.noreply.github.com>
2023-04-30 13:47:57 -0300

    pset9 done

--- lecture9/lab9/birthdays/app.py
+++ lecture9/lab9/birthdays/app.py
@@ -27,13 +27,17 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
 
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
+    
         return redirect("/")
 
     else:
-
         # TODO: Display the entries in the database on index.html
 
-        return render_template("index.html")
+        birthdays_data = db.execute("SELECT * FROM birthdays;")
 
-
+        return render_template("index.html", birthdays_data=birthdays_data)
