84923b08c741ae5fc7ca93b19ad53dd52ab6454c
jorge <62824468+jotavev@users.noreply.github.com>
2023-07-10 22:34:53 -0300

    add: dynaconf

--- lecture10/pset10/app.py
+++ lecture10/pset10/app.py
@@ -1,6 +1,10 @@
+from dynaconf import FlaskDynaconf
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+FlaskDynaconf(app)
+db = SQLAlchemy(app)
 
 
 @app.route("/")
