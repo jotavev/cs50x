be7a2f8438f899fec93b94bef66810bb6d4b1e30
jorge <62824468+jotavev@users.noreply.github.com>
2023-07-17 23:25:58 -0300

    update: register page

--- lecture10/pset10/appetieats/ext/helpers.py
+++ lecture10/pset10/appetieats/ext/helpers.py
@@ -1,5 +1,8 @@
 from functools import wraps
 from flask import redirect, session
+from appetieats.models import Users
+from appetieats.ext.database import db
+from werkzeug.security import generate_password_hash
 
 
 def login_required(f):
@@ -14,3 +17,23 @@ def login_required(f):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
+
+
+def register_user(username, password):
+    psw_hash = generate_password_hash(password)
+    new_user = Users(username=username, hash=psw_hash)
+    db.session.add(new_user)
+    db.session.commit()
+
+
+def verify_user_register_data(username, password, confirm):
+    if not username:
+        return "Insert a username"
+    elif not password:
+        return "Insert a username"
+    elif not confirm:
+        return "Insert a username"
+    elif password != confirm:
+        return "Insert a username"
+    else:
+        return "Registred!"
--- lecture10/pset10/appetieats/routes/main.py
+++ lecture10/pset10/appetieats/routes/main.py
@@ -1,5 +1,5 @@
-from flask import render_template
-from flask import Blueprint
+from flask import Blueprint, render_template, flash, request, abort, redirect
+from appetieats.ext.helpers import register_user, verify_user_register_data
 
 main_bp = Blueprint('main', __name__)
 
@@ -10,10 +10,25 @@ def index():
     return render_template("landing.html")
 
 
-@main_bp.route("/register")
+@main_bp.route("/register", methods=["GET", "POST"])
 def register():
     """Register a new restaurant"""
-    return render_template("register.html")
+    if request.method == "POST":
+        print("POST")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm = request.form.get("confirm")
+
+        a = verify_user_register_data(username, password, confirm)
+        if a == "Registred!":
+            # register_user(username, password)
+            flash(f"{a}", "success")
+            return redirect("/login")
+        else:
+            abort(403, f"{a}")
+    else:
+        print("GET")
+        return render_template("register.html")
 
 
 @main_bp.route("/login")
