b21dfddbdf4bfd01e9d41447a1c75a7890fa3a29
jorge_thinkpad <62824468+jotavev@users.noreply.github.com>
2023-07-24 13:34:55 -0300

    fix: func goPage

--- lecture10/pset10/appetieats/static/js/register.js
+++ lecture10/pset10/appetieats/static/js/register.js
@@ -5,10 +5,23 @@ function goPage(step) {
         document.querySelector("#thirdStep"),
         document.querySelector("#fourthStep")
     ];
+    const currentStep = steps[step - 2]
+
+    if (currentStep) {
+
+        const requiredFields = currentStep.querySelectorAll(".required")
+
+        for (let i = 0; i < requiredFields.length; i++) {
+            if (!requiredFields[i].reportValidity() == true) {
+                requiredFields[i].reportValidity();
+                return
+            }
+        }
+    }
+
     for (let i = 0; i < steps.length; i++) {
         steps[i].style.display = "none";
     }
-    console.log(steps)
     if (step >= 1 && step <= steps.length) {
         steps[step - 1].style.display = "block";
     } else {
