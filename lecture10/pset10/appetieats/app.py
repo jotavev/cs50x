0e283ee8f603e939ae734ccf67b133f656477883
jorge_thinkpad <62824468+jotavev@users.noreply.github.com>
2023-07-29 13:38:13 -0300

    update: admin routes

--- lecture10/pset10/appetieats/app.py
+++ lecture10/pset10/appetieats/app.py
@@ -3,6 +3,7 @@ from appetieats.ext import sesssion
 
 from appetieats.ext import database
 from appetieats.routes.main import main_bp
+from appetieats.routes.admin import admin_bp
 from appetieats.ext import commands
 from appetieats.ext import configuration
 from appetieats.ext import error
@@ -16,3 +17,4 @@ database.init_app(app)
 error.init_app(app)
 
 app.register_blueprint(main_bp)
+app.register_blueprint(admin_bp)
