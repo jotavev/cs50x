e8a5ceb74fc7e2f69b2b69571b1d4e2e4907aa20
jorge <62824468+jotavev@users.noreply.github.com>
2023-08-13 22:00:05 -0300

    update: add page

---
+++ lecture10/pset10/appetieats/ext/helper/get_inputs.py
@@ -0,0 +1,21 @@
+from flask import request
+
+
+def get_product_data_from_request():
+    data = {}
+    fields = {
+            "name": str,
+            "price": str,
+            "description": str,
+            "barcode": str,
+            "category": str
+    }
+    for field, data_type in fields.items():
+        data[field] = request.form.get(field, type=data_type)
+
+    return data
+
+
+def get_product_image(name):
+    image = request.files[f"{name}"]
+    return image
