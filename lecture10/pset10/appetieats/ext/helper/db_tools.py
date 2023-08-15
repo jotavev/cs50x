ddb38558c62462c6d68a39365e171b8bd5de9588
jorge <62824468+jotavev@users.noreply.github.com>
2023-08-14 23:12:33 -0300

    update: add page

--- lecture10/pset10/appetieats/ext/helper/db_tools.py
+++ lecture10/pset10/appetieats/ext/helper/db_tools.py
@@ -1,23 +1,36 @@
 from appetieats.models import Products, ProductImages
+from werkzeug.utils import secure_filename
 from appetieats.ext.database import db
+from flask import session
 
 
 def add_new_product(product_data, product_image):
     new_product = Products(
             name=product_data['name'],
             description=product_data['description'],
-            price=product_data['price'],
+            price=product_data['price'].replace("$ ", ""),
             available="true",
+            user_id=session.get('user_id'),
             category_id=product_data['category'],
             image_id=1
     )
     db.session.add(new_product)
     db.session.commit()
+
     product_id = new_product.id
-    image_name = f'product{product_id}';
+    print(product_id)
+
+    image_name = secure_filename(
+            f"product{product_id}.{product_image.filename.rsplit('.', 1)[1]}")
+
+    image_data = product_image.read()
+
     new_image = ProductImages(
             product_id=product_id,
             image_path=image_name,
             image_data=image_data
     )
+
+    db.session.add(new_image)
+    db.session.commit()
     return
