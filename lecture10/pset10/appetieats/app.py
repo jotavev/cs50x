e19607c891e6190179b9579cb2fff3174b97a48d
jorge_thinkpad <62824468+jotavev@users.noreply.github.com>
2023-09-01 15:12:21 -0300

    feature: restapi

--- lecture10/pset10/appetieats/app.py
+++ lecture10/pset10/appetieats/app.py
@@ -10,6 +10,7 @@ from appetieats.routes.admin.settings import settings_bp
 from appetieats.ext import commands
 from appetieats.ext import configuration
 from appetieats.ext import error
+from appetieats.routes import restapi
 
 app = Flask(__name__)
 
@@ -18,6 +19,7 @@ sesssion.init_app(app)
 configuration.init_app(app)
 database.init_app(app)
 error.init_app(app)
+restapi.init_app(app)
 
 app.register_blueprint(main_bp)
 app.register_blueprint(auth_bp)
